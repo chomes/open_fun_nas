fc8df8d74cfd2b7b5fecf4897fbd6ea5c508eca9
chomes <jaayjay@gmail.com>
2024-02-08 23:18:45 +0000

    Initial package set up
    This is the initial package deployment for open_fun_nas, it comes with
    two modules for the service install setup that will deploy plex media
    server and samba server

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # open_fun_nas
 NAS server for fun and learning
+
+
+Open fun nas is a pet project that is designed to provide a web ui for managing your nas server.  We use our opinions on how we believe the system should be configured and deploy and manage it as expected.
+
+For phase 1, we aim to have the following set up:
+
+* samba
+* plex
+* lets encrypt
+* next cloud
+* immich
+* tailscale
+
+## Requirements
+
+Users are expected to run on Ubuntu 22.04 with potential support for 24.04 when it comes out.   We will attempt to try and automate this in the future but for now you will need to install the package yourself.
---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def read_file(fname) -> str:
+    with open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="open_fun_nas", 
+    version="1",
+    author="chomes/NetRunner",
+    author_email="jaayjay@gmail.com",
+    description="Python/Ansible based NAS server management tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chomes/yt-web-ui",
+    packages=setuptools.find_packages(exclude=["test*"]),
+    install_requires=[
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
